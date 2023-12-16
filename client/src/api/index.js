815586b841880c39509be8fe84ec36cc43b2951f
avezqureshi14 <aveznqureshi14@gmail.com>
2023-12-16 20:03:17 +0530

    second commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8800" });
+const API = axios.create({ baseURL: "https://avez-voxel.onrender.com" });
 
 //api call for add startup endpoint
 export const createStartUp = (newStartup) => API.post("/startups/new",newStartup);
